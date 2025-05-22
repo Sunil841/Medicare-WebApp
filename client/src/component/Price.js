f0f4c8842a62e04b721a08f3f17b5f1016b2eb69
Sunil Kumar <sunil841032@gmail.com>
2025-05-22 15:36:31 +0530

    Add files via upload

---
+++ client/src/component/Price.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+        _id: 0,
+        name: "0 to 39",
+        array: [0, 39],
+    },
+    {
+        _id: 1,
+        name: "40 to 69",
+        array: [40, 69],
+    },
+    {
+        _id: 2,
+        name: "70 to 99",
+        array: [70, 99],
+    },
+    {
+        _id: 3,
+        name: "100 to 149",
+        array: [100, 149],
+    },
+    {
+        _id: 4,
+        name: "150 to 199",
+        array: [150, 199],
+    },
+    {
+        _id: 5,
+        name: "200 or more",
+        array: [200, 9999],
+    },
+]
